243316a7fa06802581a549e570d8f84d4e84b209
SCOUT\v.dezhurnyuk <v.dezhurnyuk@scout-gps.ru>
2024-10-19 19:23:50 +0300

    fix service worker

--- service-worker.js
+++ service-worker.js
@@ -7,11 +7,14 @@ const urlsToCache = [
   '/credit-calculator/icons/icon-512x512.png'
 ];
 
-// Установка Service Worker
+// Установка Service Worker и кэширование необходимых файлов
 self.addEventListener('install', function(event) {
+  // Пропускаем стадию ожидания(когда старый Service Worker ещё активен) и сразу активируем новый Service Worker
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
+    caches.open(CACHE_NAME).then(function(cache) {      
+      console.log('Opened cache:', CACHE_NAME);
       return cache.addAll(urlsToCache);
     })
   );
@@ -20,23 +23,31 @@ self.addEventListener('install', function(event) {
 // Активация Service Worker
 self.addEventListener('activate', function(event) {
   const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Удаление старого кэша
+            console.log('Delete old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
+
+  //Принудительная активация нового Service Worker
+  return self.clients.claim();
+
 });
 
 // Перехват запросов и кэширование
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
+      // Возвращаем ресурс из кэша, если он там есть
       if (response) {
         return response;
       }
