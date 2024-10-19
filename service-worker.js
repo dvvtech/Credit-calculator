6d40ce37cfb329965280ce905753f4926873d4ae
SCOUT\v.dezhurnyuk <v.dezhurnyuk@scout-gps.ru>
2024-10-19 19:06:44 +0300

    fix url in service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'credit-calculator-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/credit-calculator/index.html',
+  '/credit-calculator/manifest.json',
+  '/credit-calculator/icons/icon-192x192.png',
+  '/credit-calculator/icons/icon-512x512.png'
 ];
 
 // Установка Service Worker
